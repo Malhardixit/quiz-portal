a1b18a40d6b0cee2f31eaa064aa21e3078aaa8a6
Riddhiman <shubro17@outlook.com>
2023-06-19 02:33:40 +0530

    First

---
+++ models/user.mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const {qSchema} = require('./quiz.mongo')
+const usrSchema = new mongoose.Schema({
+    uid:{
+        type:String,
+        required:true
+    },
+    pwd:{
+        type:String,
+        required:true
+    },
+    quizzesConducted:{
+        type:[qSchema],
+        default:[]
+    },
+    quizzesPlayed:{
+        type:[qSchema],
+        default:[]
+    }
+})
+
+module.exports = {
+    usrSchema:usrSchema
+}
