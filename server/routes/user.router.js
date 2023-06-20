2041385351d3249295785ae7eb2985277314f5fc
Riddhiman <shubro17@outlook.com>
2023-06-20 17:11:58 +0530

    Added Quiz Registration routes

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const Router = express.Router()
 const {usrModel} = require('../models/user.mongo')
-const {createTestUsr} = require('../controllers/userControllers/controller')
+const {createTestUsr, registerForQuiz} = require('../controllers/userControllers/controller')
 Router.get('/createTestUser',createTestUsr)
+Router.post('/registerForQuiz')
+Router.post('/participateInRegisteredQuiz',registerForQuiz)
+Router.post('/saveProgress')
 module.exports = Router
