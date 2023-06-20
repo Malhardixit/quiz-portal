dfe7118b0ef158e2e1e375355daf4fb8f3127045
Anish-Parkhi <anishparkhi03@gmail.com>
2023-06-20 22:14:54 +0530

    bugs fixed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import QuizLanding from '../components/QuizLanding/QuizLanding';
 import QuizCreation from '../components/QuizCreation/QuizCreation';
 import quizLandingFlow from '../hoc/quizLandingFLow';
+import SideBar from '../components/SideBar/SideBar';
 
 function LandingPage() {
   return (
-    <div style={{ marginLeft: '7rem' }}>
-      <QuizLanding />
-      <QuizCreation />
+    // <div style={{ marginLeft: '10rem' }}>
+    <div style={{ display: 'flex' }}>
+      <div>
+        <SideBar />
+      </div>
+      <div style={{ flex: '80%' }}>
+        <QuizLanding />
+        <QuizCreation />
+      </div>
     </div>
   );
 }
