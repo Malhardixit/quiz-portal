c38e96fe67afa63324ce7350883dcfb36b4fa837
Anish-Parkhi <anishparkhi03@gmail.com>
2023-07-31 00:23:49 +0530

    Merge branch 'qbackend' of https://github.com/Malhardixit/quiz-portal into quiz-creation

--- server/app/server.js
+++ server/app/server.js
@@ -1,19 +1,26 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const http = require('http');
 const quizRouter = require('../routes/quiz.router');
 const userRouter = require('../routes/user.router');
-const mongooseUrl = "mongodb+srv://Riddhiman_Mongo:Hello123@cluster1.b76yf.mongodb.net/MyGov?retryWrites=true&w=majority";
+
+const mongooseUrl = `mongodb+srv://
+Riddhiman_Mongo:Hello123@cluster1.b76yf
+.mongodb.net/MyGov?retryWrites=true&w=majority`;
 mongoose.connect(mongooseUrl);
-console.log("Connected to db");
+console.log('Connected to db');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/user',userRouter);
-app.use('/quiz',quizRouter);
+app.use('/user', userRouter);
+app.use('/quiz', quizRouter);
+app.use(express.static('/public'));
+app.use('/build/', express.static('../../node_modules/three/build'));
+app.use('/jsm/', express.static('../../node_modules/three/examples/jsm'));
+app.use('/domevents/', express.static('../../node_modules/threex.domevents'));
 const server = http.createServer(app);
-const port = process.env.PORT||3000
-server.listen(port,()=>{
-    console.log(`Listening on ${port}...`)
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
--- server/models/quiz.mongo.js
+++ server/models/quiz.mongo.js
@@ -1,56 +1,57 @@
-const mongoose = require('mongoose')
-const {qaSchema} = require('./qa.mongo')
+const mongoose = require('mongoose');
+const { qaSchema } = require('./qa.mongo');
+
 const qSchema = new mongoose.Schema({
-    quizId:{
-        type:String,
-        required:true
-    },
-    questionSets:{
-        type:[qaSchema],
-        default:[]
-    },
-    uid:{
-        type:String,
-        required:true
-    },
-    participants:{
-        type:[{
-            uid:{
-                type:String,
-                required:true
-            },
-            correctlyAnswered:{
-                type:[qaSchema],
-                required:true,
-                default:[]
-            },
-            wronglyAnswered:{
-                type:[qaSchema],
-                required:true,
-                default:[]
-            },
-            unansweredQuestions:{
-                type:[qaSchema],
-                required:true,
-                default:[]
-            },
-            leaderboardRank:{
-                type:Number,
-                default:0
-            }
-        }],
-        default:[]
-    },
-    leaderBoard:{
-        type:[{
-            uid:String,
-            score:String
-        }],
-        default:[]
-    }
-})
+  quizId: {
+    type: String,
+    required: true,
+  },
+  questionSets: {
+    type: [qaSchema],
+    default: [],
+  },
+  uid: {
+    type: String,
+    required: true,
+  },
+  participants: {
+    type: [{
+      uid: {
+        type: String,
+        required: true,
+      },
+      correctlyAnswered: {
+        type: [qaSchema],
+        required: true,
+        default: [],
+      },
+      wronglyAnswered: {
+        type: [qaSchema],
+        required: true,
+        default: [],
+      },
+      unansweredQuestions: {
+        type: [qaSchema],
+        required: true,
+        default: [],
+      },
+      leaderboardRank: {
+        type: Number,
+        default: 0,
+      },
+    }],
+    default: [],
+  },
+  leaderBoard: {
+    type: [{
+      uid: String,
+      score: String,
+    }],
+    default: [],
+  },
+});
 
 module.exports = {
-    qSchema:qSchema,
-    quizModel:mongoose.model('quizModel',qSchema)
-}
+  qSchema,
+  quizModel: mongoose.model('quizModel', qSchema),
+};
